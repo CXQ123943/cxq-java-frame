b9d982a5580310109611aa30bd74c8373e952718
cxq <1249560518@qq.com>
2020-12-23 16:53:16 +0800

    cglib动态代理

---
+++ spring4/src/main/java/com/steven/aop/proxy/BearServiceImpl.java
@@ -0,0 +1,25 @@
+package com.steven.aop.proxy;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+public class BearServiceImpl {
+
+    public void insert() {
+        System.out.println("BearServiceImpl.insert()...");
+    }
+
+    public void select() {
+        System.out.println("BearServiceImpl.select()...");
+    }
+
+    public void update() {
+        System.out.println("BearServiceImpl.update()...");
+    }
+
+    public void delete() {
+        System.out.println("BearServiceImpl.delete()...");
+    }
+
+}
