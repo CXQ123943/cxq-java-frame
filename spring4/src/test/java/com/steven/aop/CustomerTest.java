e786b26cdcaccac93cdf5eeb0854a94859090ae4
cxq <1249560518@qq.com>
2020-12-24 16:50:42 +0800

    test测试

---
+++ spring4/src/test/java/com/steven/aop/CustomerTest.java
@@ -0,0 +1,47 @@
+package com.steven.aop;
+
+import com.steven.aop.aspect.CustomerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/aop/customer-annotation.xml")
+public class CustomerTest {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Test
+    public void beforeAndAfterAdvice() {
+        customerService.delete("zhaosi", 2);
+    }
+
+    @Test
+    public void afterBeforeAdvice() {
+        System.out.println("test: " + customerService.select("zhaosi"));
+    }
+
+    @Test
+    public void afterThrowAdvice() {
+        customerService.update(null);
+    }
+
+    @Test
+    public void aroundAdvice() {
+        System.out.println(customerService.insert(new HashMap<String, Object>()));
+    }
+
+    @Test
+    public void aroundAdviceWithException() {
+        System.out.println(customerService.insert(null));
+    }
+}
