4cb3c7fc85ad5c3441102037f5671e545976ddea
cxq <1249560518@qq.com>
2021-01-06 17:33:27 +0800

    sm整合-pojo实体类开发

---
+++ mybatis3/src/main/java/com/steven/pojo/Dog.java
@@ -0,0 +1,22 @@
+package com.steven.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dog implements Serializable {
+    private Integer id;
+    private String name;
+    private Integer gender;
+    private Integer age;
+    private String info;
+}
