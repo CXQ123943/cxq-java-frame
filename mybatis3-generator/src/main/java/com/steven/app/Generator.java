a7fec819aec37f6b526c302d57fa88402cd0ffe6
cxq <1249560518@qq.com>
2021-01-11 13:46:37 +0800

    mybatis逆向工程项目

---
+++ mybatis3-generator/src/main/java/com/steven/app/Generator.java
@@ -0,0 +1,27 @@
+package com.steven.app;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+public class Generator {
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser parser = new ConfigurationParser(warnings);
+        File generatorConfigPath = new File("mybatis3-generator/generator.xml");
+        Configuration config = parser.parseConfiguration(generatorConfigPath);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator generator = new MyBatisGenerator(config, callback, warnings);
+        generator.generate(null);
+        System.out.println("GENERATE SUCCESS...");
+    }
+}
