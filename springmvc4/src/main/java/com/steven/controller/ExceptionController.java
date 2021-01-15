97fea9990c493c8903fe6fc99b8cdcdd3efe2bff
cxq <1249560518@qq.com>
2021-01-15 14:14:18 +0800

    异常检查

---
+++ springmvc4/src/main/java/com/steven/controller/ExceptionController.java
@@ -0,0 +1,30 @@
+package com.steven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@RequestMapping("/api/exception")
+@Controller
+public class ExceptionController {
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(Integer num) {
+        System.out.println(1 / num);
+        return "ok";
+    }
+
+    @ResponseBody
+    @ExceptionHandler({ArithmeticException.class, ArrayIndexOutOfBoundsException.class})
+    public String exceptionHandler(Exception e) {
+        System.out.println("exceptionHandler()...");
+        e.printStackTrace();
+        return "err";
+    }
+}
