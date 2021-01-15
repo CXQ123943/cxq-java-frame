7fc01a21e8a90ca3df67405b9757d7fa19e2523b
cxq <1249560518@qq.com>
2021-01-15 14:12:41 +0800

    拦截器

---
+++ springmvc4/src/main/java/com/steven/interceptor/InterceptorA.java
@@ -0,0 +1,30 @@
+package com.steven.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+public class InterceptorA implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("InterceptorA: preHandle()...");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("InterceptorA: postHandle()...");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("InterceptorA: afterCompletion()...");
+    }
+}
