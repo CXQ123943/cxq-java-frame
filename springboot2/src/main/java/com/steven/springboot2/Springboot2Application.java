1023ee3fbf7e752761bc45daa5d36040e2570743
cxq <1249560518@qq.com>
2021-02-03 14:35:58 +0800

    springboot2启动类

--- springboot2/src/main/java/com/steven/springboot2/Springboot2Application.java
+++ springboot2/src/main/java/com/steven/springboot2/Springboot2Application.java
@@ -1,10 +1,20 @@
 package com.steven.springboot2;
 
+import com.steven.springboot2.servlet.ContextServlet;
+import com.steven.springboot2.servlet.fiter.ContextFilter;
+import com.steven.springboot2.servlet.listener.ContextListener;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
 
 /**
  * @author CXQ
@@ -12,12 +22,22 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 @MapperScan("com.steven.springboot2.mapper")
-public class Springboot2Application {
+@ServletComponentScan("com.steven.springboot2.servlet")
+public class Springboot2Application implements ServletContextInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot2Application.class, args);
     }
 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.addServlet("contextServlet", new ContextServlet()).addMapping("/api/servlet/context-servlet");
+        servletContext.addFilter("contextFilter", new ContextFilter())
+                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true,
+                        "/api/servlet/*");
+
+        servletContext.addListener(new ContextListener());
+    }
 }
 
 
