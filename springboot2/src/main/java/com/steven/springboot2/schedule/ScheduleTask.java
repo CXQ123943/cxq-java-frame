8d1bf5c97cd5391706e38b4451c464e0a28cad96
cxq <1249560518@qq.com>
2021-02-04 11:17:15 +0800

    springboot2-计时器

---
+++ springboot2/src/main/java/com/steven/springboot2/schedule/ScheduleTask.java
@@ -0,0 +1,21 @@
+package com.steven.springboot2.schedule;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@Component
+public class ScheduleTask {
+
+    @Scheduled(fixedDelay = 3000)
+    public void printDate() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2L);
+        System.out.println("current date: " + new Date());
+    }
+}
