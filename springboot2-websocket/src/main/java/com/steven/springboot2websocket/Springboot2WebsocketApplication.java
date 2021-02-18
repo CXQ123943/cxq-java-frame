5206714a756a8ec22a1edf43825c30ed93e890b1
cxq <1249560518@qq.com>
2021-02-18 10:40:17 +0800

    springboot2-websocket学习

---
+++ springboot2-websocket/src/main/java/com/steven/springboot2websocket/Springboot2WebsocketApplication.java
@@ -0,0 +1,17 @@
+package com.steven.springboot2websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@SpringBootApplication
+public class Springboot2WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebsocketApplication.class, args);
+    }
+
+}
---
+++ springboot2-websocket/src/test/java/com/steven/springboot2websocket/Springboot2WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steven.springboot2websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
