fdf9ae338a8a7da58ccd90c45a8977251f78ad4a
cxq <1249560518@qq.com>
2021-02-18 10:40:03 +0800

    springboot2-webflux学习

---
+++ springboot2-webflux/src/main/java/com/steven/springboot2webflux/Springboot2WebfluxApplication.java
@@ -0,0 +1,17 @@
+package com.steven.springboot2webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@SpringBootApplication
+public class Springboot2WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebfluxApplication.class, args);
+    }
+
+}
